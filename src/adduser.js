ded2de9c44afd99362a60549010d423cf74dea01
Phil <philrwebb@gmail.com>
2018-07-16 17:25:19 +0100

    validations and extra fields including binding to inner objects.

--- src/adduser.js
+++ src/adduser.js
@@ -2,14 +2,14 @@
 import * as React from 'react';
 import Link from 'valuelink';
 import {Input, isEmail} from 'valuelink/tags';
-import {isDate} from './utilities.js';
+import {isDate, isStrongPassword, isValidPostcode} from './utilities.js';
 import './App.css';
 
 const FormInput = (props) => {
     const {label, ...rest} = props;
     return (
         <tr>
-            <td>
+            <td className='labeltext'>
                 {label}
             </td>
             <td>
@@ -41,15 +41,22 @@ class AddUser extends React.Component {
         this.state = {
             name : '',
             email : '',
+            emailCheck: '',
+            password: '',
             dob: '',
+            address : {
+                line1: '',
+                line2: '',
+                suburb: '',
+                postcode: ''
+            },
             isActive : true
         };
+        console.log(this.state);
     }
 
   onSubmit = e => {
-        console.log('pressed');
-        this.setState({name: '', email: '', dob: '', isActive: true});
-        console.log(this.state);
+        this.setState({name: '', email: '', emailCheck: '', password: '',dob: '', isActive: true, address : {line1: '', line2: '', suburb: '', postcode: ''}});
         e.preventDefault();
     };
   
@@ -61,24 +68,60 @@ class AddUser extends React.Component {
     const emailLink = Link.state(this, 'email')
         .check(x => x, 'Email is required')
         .check(x => isEmail(x) ,'Must be a valid email address');
+
+    const emailCheckLink = Link.state(this,'emailCheck')
+        .check(x => x, 'Re-enter Email is required')
+        .check(x => x = this.state.email, 'Emails addresses do not match');
+
+    const passwordLink = Link.state(this, 'password')
+        .check(x => x, 'password is required')
+        .check(x => isStrongPassword(x), 'Must be 8 characters ');
     
     const dobLink = Link.state(this,'dob')
         .check(x => x, 'DOB is required')
         .check(x => isDate(x), 'Must be a valid date in format yyyy-mm-dd');
 
+    const addressLine1Link = Link.state(this,'address').at('line1')
+        .check(x => x, 'Address Line 1 is required');
+
+    const addressLine2Link = Link.state(this,'address').at('line2');
+
+    const addressSuburbLink = Link.state(this,'address').at('suburb')
+        .check(x => x, 'Suburb is required');;
+
+    const addressPostcodeLink = Link.state(this, 'address').at('postcode')
+        .check(x => x, 'Postcode is required')
+        .check(x => isValidPostcode(x), 'Postcode should be 4 digits only');
+
     const isActiveLink = Link.state(this,'isActive');
     
     return (
         <div className='divStyle'>
-            <h2>Add User</h2>
+            {/* <h2>Add User</h2> */}
             <form onSubmit={ this.onSubmit }>
                 <table>
+                    <thead>
+                        <tr />
+                        <tr>
+                            <td />
+                            <td className='formheader'>Add User</td>
+                            <td />
+                        </tr>
+                        <tr />
+                    </thead>
                     <tbody>
                         <FormInput label="Name:" valueLink={nameLink} type="text" placeholder="username" size="30" />
                         <FormInput label="Email:" valueLink={emailLink} type="text" placeholder="email" size="30" />
+                        <FormInput label="Re-Enter Email:" valueLink={emailCheckLink} type="text" placeholder="Re-enter email" size="30" />
+                        <FormInput label="Password:" valueLink={passwordLink} type="password" placeholder="password" />
                         <FormInput label="DOB:" valueLink={dobLink} type="text" placeholder="Date of Birth - yyyy-mm-dd" size="30" />
                         <FormInput label="Is active:" valueLink={isActiveLink} type="checkbox" />
-                        <FormButton buttonText="Add User" type="submit" disabled = { nameLink.error || emailLink.error || dobLink.error } />
+                        <FormInput label="Address Line 1:" valueLink={addressLine1Link} type="text" placeholder="Address Line 1" size="30" />
+                        <FormInput label="Address Line 2:" valueLink={addressLine2Link} type="text" placeholder="Address Line 2" size="30" />
+                        <FormInput label="Suburb:" valueLink={addressSuburbLink} type="text" placeholder="Suburb" size="30" />
+                        <FormInput label="Postcode:" valueLink={addressPostcodeLink} type="text" placeholder="Postcode" size="30" />                                                                        
+                        <FormButton buttonText="Add" type="submit" disabled = { nameLink.error || emailLink.error || dobLink.error } />
+                        <FormButton buttonText="Cancel" />
                     </tbody>
                 </table>             
             </form>
--- src/utilities.js
+++ src/utilities.js
@@ -1,2 +1,6 @@
 var datePattern = /^(\d{4})(\/|-)(\d{1,2})(\/|-)(\d{1,2})$/;
 exports.isDate = function (x) { return Boolean(x.match(datePattern)); };
+var validPasswordPattern = /^((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])|(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%&\/=?_.,:;\\-])|(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%&\/=?_.,:;\\-])|(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%&\/=?_.,:;\\-])).{8,}$/;
+exports.isStrongPassword = function (x) { return Boolean(x.match(validPasswordPattern)); };
+var validPostcodePattern = /^(\d{4})$/;
+exports.isValidPostcode = function (x) { return Boolean(x.match(validPostcodePattern)); };
